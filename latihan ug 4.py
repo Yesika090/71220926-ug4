a05ff7290d2f0ddf74f67c13bd127e25c5395d3b
Yesika090 <118496647+Yesika090@users.noreply.github.com>
2023-03-01 19:16:19 +0700

    Add files via upload

---
+++ latihan ug 4.py
@@ -0,0 +1,31 @@
+def aritmatika(a, b, n):
+    d = b - a
+    return (n/2) * (2*a + (n-1)*d) 
+
+
+a = int(input("Masukkan Bilangan Awal: "))
+b = int(input("Masukkan Bilangan Akhir: "))
+d = int(input("Masukkan Banyaknya Suku:"))
+n = int(input("Masukkan Total Deret: "))
+
+total = aritmatika(a, b, n)
+
+print("Total dari deret aritmatika adalah:", total)
+
+
+
+def aritmatika(a, b, n):
+    d = b - a
+    return (n/2) * (2*a + (n-1)*d) 
+
+
+a = int(input("Masukkan Bilangan Awal: "))
+b = int(input("Masukkan Bilangan Akhir: "))
+d = int(input("Masukkan Banyaknya Suku:"))
+n = int(input("Masukkan Total Deret: "))
+
+total = aritmatika(a, b, n)
+
+print("Total dari deret aritmatika adalah:", total)
+
+
